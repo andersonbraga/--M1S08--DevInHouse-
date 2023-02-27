0944f4273c01e19adcff8d5ce1c1bf24fff59744
Anderson <anderson.braga@uniplaclages.edu.br>
2023-02-26 22:54:55 -0300

    Exercicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StarWarsComponent } from './star-wars/star-wars.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, StarWarsComponent],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/personagens-star-wars.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { API_PATH } from '../environments/environments';
+import { PersonagemClass } from './personagem-class';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PersonagensStarWarsService {
+  private url = 'http://localhost:3000/personagens';
+  //private urlEnv =
+  constructor(private _httpClient: HttpClient) {}
+
+  public getPersonagens(): Observable<PersonagemClass[]> {
+    return this._httpClient.get<PersonagemClass[]>(`${API_PATH}Personagens`);
+  }
+
+  public getPersonagemURL() {
+    return this._httpClient.get<PersonagemClass[]>(this.url);
+  }
+}
